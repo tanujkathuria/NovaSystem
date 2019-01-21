d48f4793a665ff0b1d371a4235d90a31407a9a32
tanujkathuria <tanujkathuria.kathuria@gmail.com>
2019-01-22 02:34:43 +0530

    commiting test changes refactoring

--- src/test/java/com/svam/MainApplicationTests.java
+++ src/test/java/com/svam/MainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.svam.NovaSystem;
+package com.svam;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NovaSystemApplicationTests {
+public class MainApplicationTests {
 
 	@Test
 	public void contextLoads() {
